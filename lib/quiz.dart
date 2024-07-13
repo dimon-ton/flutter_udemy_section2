579831681b535332d6f6fb3075a1b5a762a2ce03
dimon-ton <saichon2namoom@gmail.com>
2024-07-13 08:07:53 +0700

    section 67 adding data model and dummy data

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -21,6 +21,12 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -32,9 +38,7 @@ class _QuizState extends State<Quiz> {
               ],
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
