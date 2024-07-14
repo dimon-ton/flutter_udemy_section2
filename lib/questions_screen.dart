aeee2adb63e3df70a8c9bf126fb8404445f56b1a
dimon-ton <saichon2namoom@gmail.com>
2024-07-14 07:44:28 +0700

    section 68 configuring column

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,13 +4,36 @@ class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
   @override
   State<QuestionsScreen> createState() {
-      return _QuestionsScreen();
+    return _QuestionsScreen();
   }
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
- @override
- Widget build(context) {
-  return const Text('questions screen.');
- }
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('questions.....'),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('answer 1'),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('answer 2'),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('answer 3'),
+          ),
+        ],
+      ),
+    );
+  }
 }
